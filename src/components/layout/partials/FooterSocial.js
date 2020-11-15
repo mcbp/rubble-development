617067e855e98739055b50d134cff514bc25d77e
Michael Priest <michael-priest@hotmail.com>
2020-11-15 21:59:15 +0000

    Added GA

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -18,12 +18,12 @@ const FooterSocial = ({
       className={classes}
     >
       <ul className="list-reset">
-        <li className="social-lg" style={{marginLeft: "12px;", marginRight: "12px"}}>
+        <li className="social-lg" style={{marginLeft: "12px", marginRight: "12px"}}>
           <a href="https://www.instagram.com/rubbledev" target="_blank" rel="noopener noreferrer">
             <FaInstagram/>
           </a>
         </li>
-        <li className="social-lg" style={{marginLeft: "12px;", marginRight: "12px"}}>
+        <li className="social-lg" style={{marginLeft: "12px", marginRight: "12px"}}>
           <a href="https://www.youtube.com/channel/UCnfOz_B12FHZd0WjOK7ZI3g/featured" target="_blank" rel="noopener noreferrer">
             <FaYoutube/>
           </a>
