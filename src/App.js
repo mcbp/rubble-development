22e3a4ef3a316ccdd7f326ec1815f3b51180f472
Michael Priest <michael-priest@hotmail.com>
2020-11-15 23:13:41 +0000

    Added GA

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import React, { useRef, useEffect } from 'react';
 import { useLocation, Switch } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
-import ReactGA from 'react-ga';
 
 // Layouts
 import LayoutDefault from './layouts/LayoutDefault';
 
 // Views
 import Home from './views/Home';
-
-// Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
-ReactGA.pageview(window.location.pathname + window.location.search)
-
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
 
 const App = () => {
 
@@ -28,7 +18,6 @@ const App = () => {
     const page = location.pathname;
     document.body.classList.add('is-loaded')
     childRef.current.init();
-    trackPage(page);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
